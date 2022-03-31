58a65ac42f2820df52b105d3c14c7cb82735f432
xyzmaker123 <xyzmaker123@protonmail.com>
2022-03-31 16:51:58 +0200

    Add org.bitcoinj.core + simple example

--- cli/src/main/java/com/xyzmake/Main.java
+++ cli/src/main/java/com/xyzmake/Main.java
@@ -1,7 +1,21 @@
 package com.xyzmake;
 
+import org.bitcoinj.core.DumpedPrivateKey;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.params.RegTestParams;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is hello world");
+        NetworkParameters params = RegTestParams.get();
+
+        DumpedPrivateKey privateKey = DumpedPrivateKey.fromBase58(params, "cVDUgUEahS1swavidSk1zdSHQpCy1Ac9XSQHkaxmZKcTTfEA5vTY");
+        ECKey key = privateKey.getKey();
+        
+        System.out.println("Private key as hex:");
+        System.out.println(key.getPrivateKeyAsHex());
+        
+        System.out.println("Public key as hex:");
+        System.out.println(key.getPublicKeyAsHex());
     }
 }
